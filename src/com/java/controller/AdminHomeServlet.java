b32f6341959a99d7fd286b64b8825aef396703fd
jedidiahbowo <jedidiahbowo@yahoo.com>
2020-04-01 11:24:25 -0500

    Implements DAO to input Bill to DB

--- src/com/java/controller/AdminHomeServlet.java
+++ src/com/java/controller/AdminHomeServlet.java
@@ -1,5 +1,10 @@
 package com.java.controller;
 
+import com.java.core.Bill;
+import com.java.exception.DatabaseException;
+import com.java.exception.NoAccountFoundException;
+import com.java.service.EBillService;
+import com.java.service.EBillServiceImpl;
 import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
@@ -12,8 +17,36 @@ import java.io.IOException;
 @WebServlet(urlPatterns = "/calculatebill", name = "AdminHomeServlet")
 public class AdminHomeServlet extends HttpServlet {
     static Logger logger = Logger.getLogger(AdminHomeServlet.class);
+
+    static EBillService eBillService;
+
+    static {
+        eBillService = new EBillServiceImpl();
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("NOT SUPPORTED.");
+        long cNum = Long.parseLong(request.getParameter("cNum"));
+        double prevRead = Double.parseDouble(request.getParameter("prevRead"));
+        double currRead = Double.parseDouble(request.getParameter("currRead"));
+
+        Bill bill = new Bill();
+        bill.setCustomerNum(cNum);
+        bill.setPrevReading(prevRead);
+        bill.setCurrReading(currRead);
+
+        String htmlResponse = "<html>";
+        try {
+            bill = eBillService.calculateBill(bill);
+        } catch (DatabaseException e) {
+            e.printStackTrace();
+        } catch (NoAccountFoundException e) {
+            e.printStackTrace();
+        }
+
+        htmlResponse += "</html>";
+
+        response.getWriter().print(htmlResponse);
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -25,11 +58,12 @@ public class AdminHomeServlet extends HttpServlet {
         }
 
         String htmlResponse = "<html><form action='calculatebill' method='post'>";
-        htmlResponse += "Consumer Number:<input type='number' required><br/>";
-        htmlResponse += "Last Month Meter Reading:<input type='number' required><br/>";
-        htmlResponse += "Current Month Meter Reading:<input type='number' required><br/>";
+        htmlResponse += "Consumer Number:<input type='number' pattern='[1-9]\\d{5}' required name='cNum'><br/>";
+        htmlResponse += "Last Month Meter Reading:<input type='number' min=\"0.00\" max=\"10000.00\" step=\"0.01\" required name='prevRead'><br/>";
+        htmlResponse += "Current Month Meter Reading:<input type='number' min=\"0.00\" max=\"10000.00\" step=\"0.01\" required name='currRead'><br/>";
         htmlResponse += "<input type='submit' value='Calculate Bill'>";
         htmlResponse += "</form></html>";
 
+        response.getWriter().println(htmlResponse);
     }
 }
--- src/com/java/dao/EBillDAOImpl.java
+++ src/com/java/dao/EBillDAOImpl.java
@@ -1,14 +1,39 @@
 package com.java.dao;
 
 import com.java.core.Bill;
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class EBillDAOImpl implements EBillDAO{
+    static Logger logger = Logger.getLogger(EBillDAOImpl.class);
     /**
      * @param bill
      * @return int 0 if successful, 1 if no account found, -1 if database exception
      */
     @Override
     public int inputBill(Bill bill) {
+        logger.info("Creating account for user.");
+        String create = "insert into BILLDETAILS(bill_num, consumer_num,cur_reading,unitConsumed,netAmount,bill_date) values(seq_bill_num.nextval,?,?,?,?,?)";
+
+        try (Connection connection = DatabaseUtil.getConnection();
+             PreparedStatement ps1 = connection.prepareStatement(create);) {
+
+            ps1.setLong(1,bill.getCustomerNum());
+            ps1.setDouble(2,bill.getCurrReading());
+            ps1.setDouble(3,bill.getUnitConsumed());
+            ps1.setDouble(4,bill.getNetAmount());
+            ps1.setDate(5,bill.getBillDate());
+
+            if ( ps1.executeUpdate() <= 0 ){
+                return 1;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+                return -1;
+        }
         return 0;
     }
 }
